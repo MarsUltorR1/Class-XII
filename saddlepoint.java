78fc8d2e6532d4c51ee70db7c7540ab4b3c4e772
MarsUltorR1 <111449923+MarsUltorR1@users.noreply.github.com>
2023-07-09 22:00:21 +0530

    Update saddlepoint.java

--- saddlepoint.java
+++ saddlepoint.java
@@ -15,7 +15,14 @@ class saddlepoint
         A=new int[N][N];
         for(int i=0;i<N;i++)
             for(int j=0;j<N;j++)
+            {
                 A[i][j]=sc.nextInt();
+                if(A[i][j]<0)
+                {
+                    System.out.println("INVALID INPUT");
+                    System.exit(0);
+                }
+            }
         for(int i=0;i<N;i++)
         {
             for(int j=0;j<N;j++)
